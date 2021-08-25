cd703bd292091be8e3475ec371034033f9652ded
bscobie <benscobie24@gmail.com>
2021-08-25 17:32:52 +1200

    add VUE login and sign up to main repo

---
+++ NZ-KendoFederation/public/vue-amplify-auth-tutorial/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { 
+  applyPolyfills,
+  defineCustomElements
+} from '@aws-amplify/ui-components/loader';
+
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+const app = createApp(App);
+app.config.compilerOptions.isCustomElement = tag => tag.startsWith('amplify-');
+app.mount('#app');
