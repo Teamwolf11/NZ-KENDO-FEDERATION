6bf13d82a09566432f9649d54484c03cb70d6cc4
bscobie <benscobie24@gmail.com>
2021-09-19 15:14:14 +1200

    Created files for displaying grading details

---
+++ NZ-KendoFederation/public/js/event.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+"use strict";
+
+// create a new module, and load the other pluggable modules
+var module = angular.module('GradingApp', ['ngResource', 'ngStorage']);
+
